05c02a41c8ec8e520bb37fee17359cfec6793af9
mia <rongm@seattleu.com>
2018-04-30 15:38:51 -0700

    hw2 5 categories

--- hw2data.js
+++ hw2data.js
@@ -2,63 +2,108 @@
   "data": [
     {
       "name": "James Bond",
-      "email": "jb@seattleu.com"
+      "email": "jb@seattleu.com",
+      "zipcode": "98101",
+      "country": "US",
+      "phone": "2061234567"
     },
     {
       "name": "Brielle Williamson",
-      "email": "bw@seattleu.edu"
+      "email": "bw@seattleu.edu",
+      "zipcode": "98101",
+      "country": "US",
+      "phone": "2062234567"
     },
     {
       "name": "Ashton Cox",
-      "email": "ac@seattleu.edu"
+      "email": "ac@seattleu.edu",
+      "zipcode": "98101",
+      "country": "US",
+      "phone": "2063234567"
     },
     {
       "name": "Airi Satou",
-      "email": "as@seattleu.edu"
+      "email": "as@seattleu.edu",
+      "zipcode": "98101",
+      "country": "US",
+      "phone": "2064234567"
     },
     {
       "name": "Caesar Vance",
-      "email": "cv@seattleu.edu"
+      "email": "cv@seattleu.edu",
+      "zipcode": "98101",
+      "country": "US",
+      "phone": "2065234567"
     },
     {
       "name": "Shad Decker",
-      "email": "Shad@seattleu.edu"
+      "email": "Shad@seattleu.edu",
+      "zipcode": "98101",
+      "country": "US",
+      "phone": "2066234567"
     },
     {
       "name": "Cedric Kelly",
-      "email": "Cedric@seattleu.edu"
+      "email": "Cedric@seattleu.edu",
+      "zipcode": "98101",
+      "country": "US",
+      "phone": "2067234567"
     },
     {
       "name": "Haley Kennedy",
-      "email": "Haley@seattleu.edu"
+      "email": "Haley@seattleu.edu",
+      "zipcode": "98101",
+      "country": "US",
+      "phone": "2068234567"
     },
     {
       "name": "Colleen Hurst",
-      "email": "Colleen@seattleu.edu"
+      "email": "Colleen@seattleu.edu",
+      "zipcode": "98101",
+      "country": "US",
+      "phone": "2069234567"
     },
     {
       "name": "Dai Rios",
-      "email": "Dai@seattleu.edu"
+      "email": "Dai@seattleu.edu",
+      "zipcode": "98101",
+      "country": "US",
+      "phone": "2061034567"
     },
     {
       "name": "Herrod Chandler",
-      "email": "Herrod@seattleu.edu"
+      "email": "Herrod@seattleu.edu",
+      "zipcode": "98101",
+      "country": "US",
+      "phone": "2061134567"
     },
     {
       "name": "Rhona Davidson",
-      "email": "Rhona@seattleu.edu"
+      "email": "Rhona@seattleu.edu",
+      "zipcode": "98102",
+      "country": "US",
+      "phone": "2061234567"
     },
     {
       "name": "Sonya Frost",
-      "email": "Sonya@seattleu.edu"
+      "email": "Sonya@seattleu.edu",
+      "zipcode": "98102",
+      "country": "US",
+      "phone": "2061144567"
     },
     {
       "name": "Jena Gaines",
-      "email": "Jena@seattleu.edu"
+      "email": "Jena@seattleu.edu",
+      "zipcode": "98122",
+      "country": "US",
+      "phone": "2061136567"
     },
     {
       "name": "Quinn Flynn",
-      "email": "Quinn@seattleu.edu"
+      "email": "Quinn@seattleu.edu",
+      "zipcode": "98108",
+      "country": "US",
+      "phone": "2061134227"
     }
 
   ]
