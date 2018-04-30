8bf900fd4a9befb03bb4eeb8d9c4abd03b68d669
mia <rongm@seattleu.com>
2018-04-30 11:45:12 -0700

    hw2 empxml

--- hw2data.js
+++ hw2data.js
@@ -7,6 +7,59 @@
     {
       "name": "Brielle Williamson",
       "email": "bw@seattleu.edu"
+    },
+    {
+      "name": "Ashton Cox",
+      "email": "ac@seattleu.edu"
+    },
+    {
+      "name": "Airi Satou",
+      "email": "as@seattleu.edu"
+    },
+    {
+      "name": "Caesar Vance",
+      "email": "cv@seattleu.edu"
+    },
+    {
+      "name": "Shad Decker",
+      "email": "Shad@seattleu.edu"
+    },
+    {
+      "name": "Cedric Kelly",
+      "email": "Cedric@seattleu.edu"
+    },
+    {
+      "name": "Haley Kennedy",
+      "email": "Haley@seattleu.edu"
+    },
+    {
+      "name": "Colleen Hurst",
+      "email": "Colleen@seattleu.edu"
+    },
+    {
+      "name": "Dai Rios",
+      "email": "Dai@seattleu.edu"
+    },
+    {
+      "name": "Herrod Chandler",
+      "email": "Herrod@seattleu.edu"
+    },
+    {
+      "name": "Rhona Davidson",
+      "email": "Rhona@seattleu.edu"
+    },
+    {
+      "name": "Sonya Frost",
+      "email": "Sonya@seattleu.edu"
+    },
+    {
+      "name": "Jena Gaines",
+      "email": "Jena@seattleu.edu"
+    },
+    {
+      "name": "Quinn Flynn",
+      "email": "Quinn@seattleu.edu"
     }
+
   ]
 }
